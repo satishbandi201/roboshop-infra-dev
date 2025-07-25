7f7d43e03202c9c0dff0ec76af8058aa22e36979
satishbandi201 <bandisatish20@gmail.com>
2025-07-25 15:45:04 +0530

    terraform roboshop

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -27,7 +27,7 @@ data "aws_ssm_parameter" "redis_sg_id" {
 }
 
 data "aws_ssm_parameter" "mysql_sg_id" {
-  name = "/roboshop/mysql/mysql_root_pass"
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
